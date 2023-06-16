bf5ec0f453dd1f1316002b49627ada9af958473c
Smitt86 <pavlov1086@ya.ruclear>
2023-06-16 18:39:33 +0300

    Added description

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Clear();
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
 Console.WriteLine("Введите трёхзначное число");
 int num = int.Parse(Console.ReadLine());
 int num3 = num % 10;
