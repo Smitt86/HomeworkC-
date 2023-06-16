bca8d16e1a3f35e75598e3a47a941042615e000f
Smitt86 <pavlov1086@ya.ru>
2023-06-16 17:58:37 +0300

    Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трёхзначное число");
+int num = int.Parse(Console.ReadLine());
+int num2 = (num /10%10);
+Console.WriteLine($"Вторая цифра в нашем трёхзначном числе -> {num2}");
