9700173d4c51f2777ce590026953e4db01b9b986
Smitt86 <pavlov1086@ya.ruclear>
2023-06-16 18:33:13 +0300

    task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трёхзначное число");
+int num = int.Parse(Console.ReadLine());
+int num3 = num % 10;
+if (num < 100) Console.WriteLine("Вы ввели не трёхзначное число");
+else Console.WriteLine($"Третья цифра в трёхзначном числе - > {num3}");
