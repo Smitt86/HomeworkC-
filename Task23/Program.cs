0fb36e15d2664e679a2e8a8a45ca2a61f56ad4e7
Smitt86 <pavlov1086@ya.ruclear>
2023-06-17 17:00:20 +0300

    Task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введите число для возведения в третью степень: ");
+int value = int.Parse(Console.ReadLine()!);
+for (int i = 1; i <=value; i++)
+    {
+        Console.Write($"{i} в кубе равно - >  ");
+        Console.WriteLine(Math.Pow(i,3));
+    }
