cbccf9242cb778768c707e0a76af3fe61a3487da
Smitt86 <pavlov1086@ya.ruclear>
2023-06-23 20:05:34 +0300

    Task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+Console.WriteLine("Ищем сумму цифр одного введенного числа");
+Console.WriteLine("Введите число!");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = Summirovanie(number);
+Console.WriteLine($"Сумма цифр числа{number} равна {result}");
+
+int Summirovanie(int num)
+{
+int sum = 0;
+while (num !=0)
+{sum = sum + num%10;
+num = num/10;
+}
+return sum;
+}
