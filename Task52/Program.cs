a417d402bd7b2b3ed366b7dd95bf560cce22b55f
Smitt86 <pavlov1086@ya.ruq>
2023-07-04 00:06:31 +0300

    Task 52

---
+++ Task52/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine()!);
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine()!);
+int[,] array = GetArray(rows, columns, 1, 10);
+PrintArray(array);
+int[] summa = SumColumns(array);
+SredArifm(summa);
+
+void SredArifm(int[] arr)
+{
+    double[] sum = new double[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum[i] += sum[i] / arr.Length;
+    }
+    WriteLine(String.Join(" ", sum));
+}
+
+int[] SumColumns(int[,] arr)
+{
+    int[] sum = new int[arr.GetLength(0)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sum[i] = sum[i] + arr[j, i];
+        }
+    }
+    return sum;
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
